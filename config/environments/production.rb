55e054eeb45ef59df6653bfd557b778a8a2b5d49
Ben Lerchin <ben@lerchin.com>
2014-02-21 21:28:23 -0800

    change hostname to submit.wallapopup.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ FullView::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-	config.action_mailer.default_url_options = { :host => "http://popup-app.herokuapp.com" }
+	config.action_mailer.default_url_options = { :host => "http://submit.wallapopup.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
