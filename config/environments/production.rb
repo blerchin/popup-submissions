2fa1681648df3d4fd57c774d35ba238abef3b8e3
Ben Lerchin <ben@lerchin.com>
2014-02-21 20:28:35 -0800

    added default url for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,9 @@ FullView::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+	config.action_mailer.default_url_options = { :host => "http://popup-app.herokuapp.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -77,4 +79,5 @@ FullView::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
 end
